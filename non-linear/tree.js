e52eab3e88756c0ae6a3c873ac9ae969c538ec03
DavMaish <davihmaish98@gmail.com>
2026-02-16 07:14:09 +0300

    Update tree.js

--- non-linear/tree.js
+++ non-linear/tree.js
@@ -1,1 +1,25 @@
-//tree implementation
+// Tree Data Structure Implementation
+
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
+
+  addChild(node) {
+    this.children.push(node);
+  }
+}
+
+// Creating a simple tree
+const root = new TreeNode("Root");
+
+const child1 = new TreeNode("Child 1");
+const child2 = new TreeNode("Child 2");
+
+root.addChild(child1);
+root.addChild(child2);
+
+console.log("Root Node:", root.value);
+console.log("Children of Root:", root.children.map(child => child.value));
+
