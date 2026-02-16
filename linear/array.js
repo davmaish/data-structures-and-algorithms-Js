1a9dc7eebf93402e737a2668d93941000173b9d5
DavMaish <davihmaish98@gmail.com>
2026-02-16 07:09:21 +0300

    Update array.js

--- linear/array.js
+++ linear/array.js
@@ -1,1 +1,26 @@
-// Array implementation will go here
+// Array Data Structure Implementation
+
+// Creating an array
+let numbers = [10, 20, 30, 40];
+
+// Insertion
+numbers.push(50);
+
+// Traversing
+console.log("Traversing Array:");
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+// Searching using Linear Search
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log("Index of 30:", linearSearch(numbers, 30));
+
