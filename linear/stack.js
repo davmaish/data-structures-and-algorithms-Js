05e2d7aa2741e66b4072eafd1707bb5babfa7763
DavMaish <davihmaish98@gmail.com>
2026-02-16 07:10:24 +0300

    Update stack.js

--- linear/stack.js
+++ linear/stack.js
@@ -1,1 +1,31 @@
-// Stack implementation
+// Stack Data Structure (LIFO)
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack Underflow";
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
+
+const stack = new Stack();
+stack.push(100);
+stack.push(200);
+console.log("Popped:", stack.pop());
