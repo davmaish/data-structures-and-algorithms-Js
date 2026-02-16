388d3e2d599bd8bbda03a20de69113f717b62e02
DavMaish <davihmaish98@gmail.com>
2026-02-16 07:17:48 +0300

    Update graph.js

--- non-linear/graph.js
+++ non-linear/graph.js
@@ -1,1 +1,35 @@
-//graph implementation
+// Graph Data Structure Implementation (Adjacency List)
+
+class Graph {
+  constructor() {
+    this.adjList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjList[vertex]) {
+      this.adjList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.adjList[v1].push(v2);
+    this.adjList[v2].push(v1);
+  }
+
+  display() {
+    console.log(this.adjList);
+  }
+}
+
+// Creating a simple graph
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+
+graph.display();
+
