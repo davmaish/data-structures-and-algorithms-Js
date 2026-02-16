c7917a9127ac04f7478eb544369ae7b57e199c59
DavMaish <davihmaish98@gmail.com>
2026-02-16 07:12:20 +0300

    Update linkedList.js

--- linear/linkedList.js
+++ linear/linkedList.js
@@ -1,1 +1,44 @@
-// linkedlist implementation
+// Linked List Data Structure
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  printList() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const list = new LinkedList();
+list.append(5);
+list.append(10);
+list.printList();
+
